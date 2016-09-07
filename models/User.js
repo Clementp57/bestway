e303a19d416c3137b840f90e675e3afed293be5b
Clément Peyrabere <Clem@MacBook-Pro-de-Clement-3.local>
2016-09-07 16:56:30 +0200

    added scheduler

---
+++ models/User.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var User = new mongoose.Schema({
+        id: String,
+        name: String,
+        age: Number,
+        sexe: String
+    });
+
+module.exports = mongoose.model('User', User);
---
+++ scripts/massInsert.sh
@@ -0,0 +1,1 @@
+ab -p post_user.txt -T application/json -c 30 -n 100000 http://127.0.0.1/api/v1/users/
