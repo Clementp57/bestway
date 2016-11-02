3e0b1b33cc987a7a2e9b439c44fdbd4b54b61c90
Clément Peyrabere <Clem@mbpdeclement3.home>
2016-11-02 21:37:50 +0100

    clean code & use promises

--- routes/transports.js
+++ routes/transports.js
@@ -18,14 +18,14 @@ var transportationTypesArray = [
   { id: 7, type: "transit", subtype: "rail" }
 ]
 
-function getTransportationInformations(transportType, transportSubtype, departureLocation, arrivalLocation)  {
+function getTransportationInformations(transportType, transportSubtype, departureLocation, arrivalLocation) {
   var BASE_GOOGLE_MAP_URL = "https://maps.googleapis.com/maps/api/directions/json?key=AIzaSyD3MxmmqvuEMX5090XYlxTnveg-ZDlNXP0";
   var requestURL = BASE_GOOGLE_MAP_URL;
   requestURL += "&origin=" + departureLocation.latitude + "," + departureLocation.longitude;
   requestURL += "&destination=" + arrivalLocation.latitude + "," + arrivalLocation.longitude;
-  
+
   var transport;
-  if(transportType == "walking" || transportType == "bicycling" || transportType == "driving") {
+  if (transportType == "walking" || transportType == "bicycling" || transportType == "driving") {
     transport = transportType;
   } else {
     transport = transportSubtype;
@@ -43,9 +43,9 @@ function getTransportationInformations(transportType, transportSubtype, departur
         var object = JSON.parse(json);
         var result = { transport: transport, duration: object.routes[0].legs[0].duration.value };
         resolve(result);
-        
+
       }).on('error', (error) => {
-          reject(error);
+        reject(error);
       })
     });
   });
@@ -53,7 +53,7 @@ function getTransportationInformations(transportType, transportSubtype, departur
 }
 
 router.post('/', function (req, res) {
-  
+
   var departureLocation = req.body.departure;
   var arrivalLocation = req.body.arrival;
 
@@ -68,24 +68,38 @@ router.post('/', function (req, res) {
 
   var promise = new Promise((resolve, reject) => { resolve(null) });
   transportationTypesArray.forEach(function (transportId, index) {
-    promise = promise.then(function(res) {
+    promise = promise.then(function (res) {
       console.log("res => ", res);
-      if(res != null) {
+      if (res != null) {
         responseArray.push(res);
       }
       return getTransportationInformations(transportId.type, transportId.subtype, departureLocation, arrivalLocation);
-    });    
+    });
   });
 
-  promise.then(function() {
-//         UserTransportationPreferences.findOne({ 'userId': req.headers['x-user-id'] }, 'bike  bus walk subway car', function (err, preference) {
-  //           if (err || !preference) {
-  //             // No preferences
+  promise.then(function () {
+    //         UserTransportationPreferences.findOne({ 'userId': req.headers['x-user-id'] }, 'bike  bus walk subway car', function (err, preference) {
+    //           if (err || !preference) {
+    //             // No preferences
 
-  //           } else {
+    //           } else {
 
-  //           }
-  //         });
+    //           }
+    //         });
+    // TMP
+    for (var k = 0; k < responseArray.length; k++) {
+      if (responseArray[k].transport == "walking") {
+        responseArray[k].praticalIndex = 0;
+      } else if (responseArray[k].transport == "bicycling") {
+        responseArray[k].praticalIndex = 1;
+      } else if (responseArray[k].transport == "driving") {
+        responseArray[k].praticalIndex = 4;
+      } else if (responseArray[k].transport == "bus") {
+        responseArray[k].praticalIndex = 3;
+      } else if (responseArray[k].transport == "subway") {
+        responseArray[k].praticalIndex = 2;
+      }
+    }
 
     res.status(200).json({
       response: { responseArray }
